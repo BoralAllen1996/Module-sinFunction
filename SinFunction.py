2d55cac80e3af89a47cf4b2a83e28771e30e9757
lenbo <allenbora@gmail.com>
2022-11-10 23:36:28 +0800

    nineth

---
+++ SinFunction.py
@@ -0,0 +1,10 @@
+#Python code demonstrating the math module sin ========
+#importing math for operation======
+import math
+
+a = math.pi / 6
+
+# Returning the value of sine of pi / 6
+print("\n\nThe value of sine of pi / 6 is : ", end ="")
+
+print(math.sin(a))
